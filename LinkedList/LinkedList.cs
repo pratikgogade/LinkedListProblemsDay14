216dbe54de95d5ed7410eabc4901d0a83453cbbd
unknown <Pratik@Example.com>
2022-12-22 01:40:12 +0530

    [Pratik]Add:Simple Linked List

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedList
+{
+    internal class LinkedList
+    {
+        Node head;
+        public void Addnote(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine("{0} inserted into Linked List", node.data);
+        }
+        public void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is empty");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("List contains");
+                Console.ResetColor();
+                Node tempnode = head;
+                while (tempnode != null)
+                {
+                    Console.WriteLine("Data - " + tempnode.data + " ");
+                    tempnode = tempnode.Next;
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Addnote(56);
+            list.Addnote(30);
+            list.Addnote(70);
+            list.Display();
+
+        }
+    }
+}
